ef3430815e74fda69df2a6ab1a4eca8b565a2938
Jay Patel <jaypatel@Jays-MacBook-Air.local>
2018-10-23 10:09:19 +1100

    StripePaymentGateway

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Stripe;
+using StripePayments.Models;
+
+namespace StripePayments.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Charge()
+        {
+            ViewBag.Message = "Learn how to process payments with Stripe";
+            return View(new StripeChargeModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Charge(StripeChargeModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var chargeId = await ProcessPayment(model);
+            return View("Index");
+        }
+
+        private async Task<string> ProcessPayment(StripeChargeModel model)
+        {
+           
+                //StripeConfiguration.SetApiKey("pk_test_ZwQQ3DpHRcIWDMKgVObfuSYl");
+                return await Task.Run(() =>
+                {
+                    var myCharge = new StripeChargeCreateOptions
+                    {
+                        Amount = (int)(model.Amount * 1),
+                        Currency = "gbp",
+                        Description = "Description for test charge",
+                        // SourceTokenOrExistingSourceId = model.Token
+                    };
+                    try
+                    {
+                        var chargeService = new StripeChargeService("sk_test_sunSx6HuXZrAcIp2W8k7L4zk");
+                        var stripeCharge = chargeService.Create(myCharge);
+                        return stripeCharge.Id;
+                    }
+                    catch (Exception e)
+                    {
+                        Response.Write("<script>alert('Data Saved succesfully in Database but further transaction process  you must provide Cutomer or merchant.')</script>");
+                        return e.Message;
+                    }     
+                });
+       
+           
+        }
+
+        [HttpPost]
+        public JsonResult AddUserCardInfo(tblStripe stripe)
+        {
+            StripeChargeModel sm = new StripeChargeModel();
+            int op = sm.AddUserCardInfo(stripe);
+            return Json(op,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Models/StripeChargeModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using StripePayments.Models;
+
+namespace StripePayments.Models
+{
+    public class StripeChargeModel
+    {
+        [Required]
+        public string Token { get; set; }
+        [Required]
+        public double Amount { get; set; }
+
+
+        //optional fields
+        public string CardHolderName { get; set; }
+        public string cardno { get; set; }
+        public string expDate { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressCity { get; set; }
+        public string AddressPostcode { get; set; }
+        public string AddressCountry { get; set; }
+
+
+
+        public int AddUserCardInfo(tblStripe model)
+        {
+            Database1Entities1 db = new Database1Entities1();
+            
+            try
+            {
+                db.tblStripes.Add(model);
+                db.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
